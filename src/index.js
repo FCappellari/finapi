b428c64cbbbbecb59b24978cd4948785d8db2317
Fabricio Cappellari <fabricio.cappellari@totvs.com.br>
2021-12-06 17:19:59 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists!" });
+    }
+    
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return res.status(201).send();
+});
+
+app.get("/statement/:cpf", (req, res) => {
+    const { cpf } = req.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement);
+})
+
+app.listen(3333);
